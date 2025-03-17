a81456f5ead38f4bddb3870f5bb29ca9102b7472
LuisFXZ <luisflores240903@gmail.com>
2025-03-17 17:51:59 -0500

    feat(gateway): :sparkles: add function send message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [ChatModule],
+  imports: [
+    ChatModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,16 +1,68 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { OnModuleInit } from '@nestjs/common';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatGateway implements OnModuleInit {
 
+  @WebSocketServer()
   public server: Server
 
   constructor(private readonly chatService: ChatService) { }
 
-  onModuleInit() { 
+  onModuleInit() {
+
+    this.server.on('connection', (socket: Socket) => {
+
+      const { name, token } = socket.handshake.auth;
+      if (!name) {
+        socket.disconnect();
+        return;
+      }
+
+      this.chatService.onClientConnected({
+        id: socket.id,
+        name: name
+      });
+
+      // socket.emit('welcome-message', 'Welcome to the chat');
+
+      this.server.emit('on-clients-changed', this.chatService.getClients());
+
+      socket.on('disconnect', () => {
+        this.chatService.onClientDisconnected(socket.id);
+        this.server.emit('on-clients-changed', this.chatService.getClients());
+        // console.log('Client disconnected:', socket.id);
+      });
+
+    });
+
+  }
+
+  @SubscribeMessage('send-message')
+  handleMessage(
+    @MessageBody() message: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+
+    const { name, tocken } = client.handshake.auth;
+    // console.log({ name, message });
+
+    if (!message) {
+      return;
+    }
+
+    this.server.emit(
+      'on-message',
+      {
+        userId: client.id,
+        message: message,
+        name: name,
+      }
+    )
+
+
 
   }
 
